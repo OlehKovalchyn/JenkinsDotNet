50c0009ec0113be07421c2b4617d5cacf9d46677
OlehKovalchyn <49445002+OlehKovalchyn@users.noreply.github.com>
2019-06-18 17:04:38 +0300

    Update Hello.cs

--- Hello.cs
+++ Hello.cs
@@ -6,10 +6,6 @@ namespace HelloWorld
         static void Main() 
         {
             Console.WriteLine("Hello World!");
-
-            // Keep the console window open in debug mode.
-            Console.WriteLine("Press any key to exit.");
-            Console.ReadKey();
         }
     }
 }
